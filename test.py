6396700e62f9a9d87b892147a9d3e1cc3a754064
Ray <rwstorer@hotmail.com>
2023-01-30 13:19:44 -0500

    testing

---
+++ test.py
@@ -0,0 +1,29 @@
+"""
+Name     : Your name goes here
+File Name: m02_lab_case_study.py
+Summary  : Summary of code purpose goes here
+"""
+
+NAME_PROMPT: str = 'Enter your name'
+GPA_PROMPT: str = 'Enter your GPA'
+MIN_NAME_LEN: int = 3
+MIN_NAME_LEN_ERR: str = f"Minimum name length is: {MIN_NAME_LEN}"
+TRY_AGAIN_ERR: str = 'Please try again'
+FLOAT_ERR: str = 'Please input a floating point number'
+
+user_name: str = ''
+while True:
+    user_name = input(NAME_PROMPT)
+    if len(user_name) >= MIN_NAME_LEN:
+        break
+    else:
+        print(MIN_NAME_LEN_ERR)
+        print(TRY_AGAIN_ERR)
+
+while True:
+    try:
+        gpa: float = float(input(GPA_PROMPT))
+        break
+    except:
+        print(FLOAT_ERR)
+
