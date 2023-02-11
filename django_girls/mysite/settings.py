7f95a49e176ee549c6ba472b4648a475f056a753
Ray <rwstorer@hotmail.com>
2023-02-10 21:30:28 -0500

    adding_changed_files

--- django_girls/mysite/settings.py
+++ django_girls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6wihs1dm6&@&9-&hj55ldoc-xb5kt$3r7m@&(-j!t%j71u)$j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
