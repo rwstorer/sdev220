2e9360c10fc22c868256247614fbe5e1cec4b2a8
Ray <rwstorer@hotmail.com>
2023-02-22 21:00:14 -0500

    sqlalchemy

---
+++ sqlalchemy/m04_sqlalchemy.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+
+engine = sa.create_engine('sqlite:///books.db')
+conn = engine.connect()
+sql = sa.text('SELECT `title` from `book`')
+rows = conn.execute(sql)
+
+for row in rows:
+    print('title:', row[0])
+
+conn.close()
+engine.dispose()
