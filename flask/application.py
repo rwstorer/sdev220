c80512cc8c5ecf719f027abc1e7213009bf59fc8
Ray <rwstorer@hotmail.com>
2024-02-12 19:50:21 -0500

    flask

---
+++ flask/application.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True)
+    author = db.Column(db.String(80))
+    publisher = db.Column(db.String(80))
+    
+    def __repr__(self) -> str:
+        return f"id {self.id}, book_name {self.book_name}, " + \
+            f"author {self.author} publisher {self.publisher}"
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+
+    output = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+        }
+        output.append(book_data)
+
+    return {'books': output}
+
+
+@app.route('/books', methods=['POST'])
+def create_book() -> str:
+    book = Book()
+    book.id = request.json['id']
+    book.book_name = request.json['book_name']
+    book.author = request.json['author']
+    book.publisher = request.json['publisher']
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
